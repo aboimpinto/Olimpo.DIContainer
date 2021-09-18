e6e872e027dd8d0922090bca9c450ed2f29d5279
Paulo Aboim Pinto <aboimpinto@gmail.com>
2021-09-19 00:12:14 +0200

    ServiceLocator implementation

---
+++ Olimpo.DIContainer/IServiceLocator.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Olimpo.DIContainer
+{
+    public interface IServiceLocator
+    {
+        ServiceCollection ServiceCollection { get; set; }
+
+        ServiceProvider ServiceProvider { get; }
+    }
+}
---
+++ Olimpo.DIContainer/ServiceLocator.cs
@@ -0,0 +1,31 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Olimpo.DIContainer
+{
+    public class ServiceLocator : IServiceLocator
+    {
+        private static IServiceLocator _current = new ServiceLocator();
+
+        public ServiceCollection ServiceCollection { get; set; }
+
+        public ServiceProvider ServiceProvider => this.ServiceCollection.BuildServiceProvider();
+
+        public static IServiceLocator Current
+        {
+            get
+            {
+                return _current;
+            }
+        }
+
+        static ServiceLocator()
+        {
+            _current.ServiceCollection.AddSingleton<IServiceLocator>(_current);
+        }
+
+        public ServiceLocator()
+        {
+            this.ServiceCollection = new ServiceCollection();
+        }
+    }
+}
